04ee7d1733f03c96fb3defe1c489c9b0a6b7f97f
project-demo <nitishsharma2703@gmail.com>
2026-05-18 18:11:24 +0530

    fixed modal filename case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import {Routes , Route} from "react-router";
+import { Routes, Route } from "react-router";
+
 import Login from "./routes/login";
 import Register from "./routes/register";
 import Protected from "./compounds/protected";
 import SplitView from "./components/SplitView";
 import Hero3D from "./routes/hero3D";
-import NewModal from "./components/NewModal";
-
-
-
-// Home page now uses SplitView with animations
+import NewModal from "./components/NewModal.jsx";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Routes>
         <Route path="/login" element={<Login />} />
+
         <Route path="/register" element={<Register />} />
-        <Route path="/s" element={<Protected><h1>Welcome to the App</h1></Protected>} />
+
+        <Route
+          path="/s"
+          element={
+            <Protected>
+              <h1>Welcome to the App</h1>
+            </Protected>
+          }
+        />
+
         <Route path="/" element={<Hero3D />} />
+
         <Route path="/newmodal" element={<NewModal />} />
-        
-      
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
